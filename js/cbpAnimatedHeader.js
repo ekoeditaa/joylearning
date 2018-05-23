4406699e04aaa86d5cae4ea4cdfc48364b92839a
ekoeditaa <ekoeditaa@gmail.com>
2018-05-23 14:10:00 +0800

    add smooth scrolling

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -42,3 +42,13 @@ var cbpAnimatedHeader = (function() {
 	init();
 
 })();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
